f51cbe019b8b279111eaa1c078089dee26c40289
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-06-13 12:04:28 +0100

    Exercise 2.4

--- part 2 Communicating with server/exercises/courseinfo/src/App.jsx
+++ part 2 Communicating with server/exercises/courseinfo/src/App.jsx
@@ -1,38 +1,58 @@
 import { useState } from "react"
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      },
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   const [total, setTotal] = useState(0)
 
   return (
     <div>
-      <Course course={course}/> 
+      {/* <Course course={course}/>  */}
+      {courses.map(course => <Course key={course.id} course={course}></Course>)}
     </div>
   )
 }
@@ -70,7 +90,7 @@ const Part = ({id, name, exercises}) => {
 
 const Total = ({total}) => {
   return (
-    <h3>total of {total} exercises</h3>
+    <h4>total of {total} exercises</h4>
   )
 }
 
