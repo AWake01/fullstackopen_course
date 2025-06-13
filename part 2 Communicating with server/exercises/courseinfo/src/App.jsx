c206d7b5ec4eb3eca1d8961de7a17c12f72f7996
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-06-13 12:00:27 +0100

    Exercise 2.2 finished

--- part 2 Communicating with server/exercises/courseinfo/src/App.jsx
+++ part 2 Communicating with server/exercises/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const App = () => {
   const course = {
     id: 1,
@@ -18,17 +20,36 @@ const App = () => {
         exercises: 14,
         id: 3
       },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      },
     ]
   }
 
-  return <Course course={course} />
+  const [total, setTotal] = useState(0)
+
+  return (
+    <div>
+      <Course course={course}/> 
+    </div>
+  )
 }
 
 const Course = ({course}) => {
+  const calcTotal = ({course}) => {
+    const content = course.parts
+    const newTotal = content.reduce((acc, part) => acc + part.exercises, 0)
+    console.log(newTotal)
+    return newTotal
+  }
+
   return (
     <div>
       <h1>{course.name}</h1>
-      <Content content={course.parts}></Content>
+      <Content content={course.parts}/>
+      <Total total={calcTotal({course})}/>
     </div>
   )
 }
@@ -44,6 +65,12 @@ const Content = ({content}) => {
 const Part = ({id, name, exercises}) => {
   return (
     <p>{name} {exercises}</p>
+  )
+}
+
+const Total = ({total}) => {
+  return (
+    <h3>total of {total} exercises</h3>
   )
 }
 
