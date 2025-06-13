c2ac2b64ac0e87ac7120ccd302f3b6caded8dc7e
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-06-13 22:38:38 +0100

    Exercise 2.9 Start

--- part 2 Communicating with server/exercises/phonebook/src/App.jsx
+++ part 2 Communicating with server/exercises/phonebook/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '11-11-111111' },
-    { name: 'Joe Blogs', number: '22-22-222222' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState([])
 
   const addClick = (event) => {
     event.preventDefault()
@@ -22,7 +25,7 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    const newName = event.target.value
+    const newName = event.target.values
     setNewName(newName)
   }
 
@@ -31,9 +34,15 @@ const App = () => {
     setNewNumber(newNumber)
   }
 
+   const handleSearchChange = (event) => {
+    setFilteredPersons(persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase())))  //Case sensitvie
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>name: <input onChange={handleSearchChange}/></div>
+      <h2>Add a new</h2>
       <form onSubmit={addClick}>
         <div>name: <input onChange={handleNameChange}/></div>
         <div>number: <input onChange={handleNumberChange}/></div>
@@ -42,7 +51,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Number key={person.name} name={person.name} number={person.number}/>)} 
+      {filteredPersons.map(person => <Number key={person.name} name={person.name} number={person.number}/>)} 
     </div>
   )
 }
