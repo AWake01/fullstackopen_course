cdcdec57ff55e87dab80910ac41716d8940f5f00
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-06-13 17:51:04 +0100

    Exercise 2.6

--- part 2 Communicating with server/exercises/phonebook/src/App.jsx
+++ part 2 Communicating with server/exercises/phonebook/src/App.jsx
@@ -2,24 +2,44 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' },
+    { name: 'Joe Blogs' }
   ]) 
   const [newName, setNewName] = useState('')
+
+  const addClick = (event) => {
+    event.preventDefault()
+    console.log(newName)
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handleNameChange = (event) => {
+    const newName = event.target.value
+    setNewName(newName)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addClick}>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <Number key={person.name} name={person.name}/>)} 
     </div>
+  )
+}
+
+const Number = ({name}) => {
+  return( 
+    <p>{name}</p>
   )
 }
 
